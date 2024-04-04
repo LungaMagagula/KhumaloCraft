381e927225f205b9e17ea35d69404f7886a33bd4
LungaMagagula <lungamagagula8@gmail.com>
2024-04-04 20:48:56 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KhumaloCraft.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+            public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult MyWork()
+        {
+            return View();
+        }
+
+    }
+}
